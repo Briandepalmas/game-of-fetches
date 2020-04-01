51c9b1a50ce800eb01e1495300701377eedd20ca
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-01 12:00:25 -0400

    delete default app and pasted axios code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isLoaded: false,
+      data: []
+    }
+  }
+
+  componentDidMount() {
+    // |||||||||||||| fetch |||||||||||||||
+    // fetch("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+    //   .then(response => response.json()) // first response => resolved
+    //   .then(
+    //     result => {
+    //       console.log(result.results)
+
+    //       this.setState({
+    //         isLoaded: true,
+    //         items:result.results
+    //       })
+
+    //       console.log("state is updated",this.state.data)
+    //     }
+    //   ).catch(e => console.log("there's a error", e))
+    // |||||||||||||||||||||||||||||||||||||
+
+    axios.get("https://api.spoonacular.com/recipes/search?query=cheese&number=4&apiKey=27a02bbb5b48401f96bfda6a7d3e2545")
+    .then(res => {
+      const recipes = res.data.results;
+      console.log("recipes ---->", recipes)
+      
+      this.setState({data: recipes})
+
+    })
+    .catch(error => {
+      console.log('there is an eror', error)
+    })
+
+  }
+
+
+  render() {
+    return (
+      <div>
+        <h1> Spoonacular API </h1>
+        <ul>
+          {this.state.data.map((recipe, id) => <li key={id}> {recipe.title} </li>)}
+        </ul>
+      </div>
+    )
+  }
 }
-
-export default App;
