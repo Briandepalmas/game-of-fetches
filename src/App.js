b749d4af5ab591f670a9969144f6491a36795b13
Briandepalmas <Student_GA110@Student-GA110.home>
2020-04-01 23:17:04 -0400

    filtered all data and assigned it to DOM except last 2

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ export default class App extends Component {
 
     let one = "http://anapioficeandfire.com/api/characters/16"
     let two = "http://www.anapioficeandfire.com/api/houses/37"
-    let three = "https://api.storyblok.com/v1/cdn/stories/vue?version=published&token=wANpEQEsMYGOwLxwXQ76Ggtt"
+    let three = "http://www.anapioficeandfire.com/api/houses/229"
     let four = "http://www.anapioficeandfire.com/api/houses/17"
     let five = "http://www.anapioficeandfire.com/api/characters/901"
     let six= "http://www.anapioficeandfire.com/api/houses/362"
@@ -29,26 +29,32 @@ export default class App extends Component {
     const requestOne = axios.get(one);
     const requestTwo = axios.get(two);
     const requestThree = axios.get(three);
-    const requestFour = axios.get(three);
-    const requestFive = axios.get(three);
-    const requestSix = axios.get(three);
-    const requestSeven = axios.get(three);
+    const requestFour = axios.get(four);
+    const requestFive = axios.get(five);
+    const requestSix = axios.get(six);
+    const requestSeven = axios.get(seven);
     
     axios.all([requestOne, requestTwo, requestThree]).then(axios.spread((...responses) => {
       const responseOne = responses[0]
       const responseTwo = responses[1]
-      const responesThree = responses[2]
+      const responseThree = responses[2]
+      const responseFour = responses[3]
+      const responseFive = responses[4]
+      const responseSix = responses[5]
+      const responseSeven = responses[6]
       // use/access the results 
 
       let a=responseOne.data.born
       let b=responseTwo.data.region
-      let c=responseTwo.data.region
-      let d=responseTwo.data.region
-      let e=responseTwo.data.region
-      let f=responseTwo.data.region
-      let g=responseTwo.data.region
+      let c=responseThree.data.coatOfArms
+      let d=responseFour.data.seats[1]
+      let e=responseFive.data.aliases[1]
+      let f=responseSix.data.founder //"https://www.anapioficeandfire.com/api/characters/209"
+      let g=responseSeven.data.povBooks // "https://www.anapioficeandfire.com/api/books/1",
+                                        // "https://www.anapioficeandfire.com/api/books/2",
+                                        // "https://www.anapioficeandfire.com/api/books/3"
       
-      this.setState({data: [mary,bob]})
+      this.setState({data: [a,b,c,d,e,f,g]})
       //this.setState({data: bob})
     })).catch(errors => {
       // react on errors.
@@ -66,15 +72,15 @@ export default class App extends Component {
         <h1>What region is House Targaryen in?</h1> 
         {this.state.data[1]}
         <h1>What's the coat of arms of House Lannister?</h1>
-        {this.state.data[1]} 
+        {this.state.data[2]} 
         <h1>What is the second seat of House Baratheon?</h1> 
-        {this.state.data[1]}
+        {this.state.data[3]}
         <h1>What is Robert Baratheon's second alias?</h1> 
-        {this.state.data[1]}
+        {this.state.data[4]}
         <h1>What's the name of the founder of House Stark?</h1> 
-        {this.state.data[1]}
+        {this.state.data[5]}
         <h1>What are the titles of Catelyn Stark's three POV books?</h1> 
-        {this.state.data[1]}
+        {this.state.data[6]}
         </div>
     
     )
